b15aa74f934bfc3a128607b62d1e7c7014b39d24
Yakult Yang <k@abreto.net>
2019-05-24 16:03:54 +0800

    try client route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3001;
@@ -7,6 +8,9 @@ app.get('/api', (req, res) => res.send('API.'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 app.listen(port, () => console.log(`Test on port ${port}`));
