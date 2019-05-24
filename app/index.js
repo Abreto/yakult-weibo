eae72981408c45bf2415b677b32273c46099984e
Yakult Yang <k@abreto.net>
2019-05-24 17:07:23 +0800

    hot reload

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
-app.get('/api', (req, res) => res.send('API.'));
+app.get('/api', (req, res) => res.send('API Route.'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build'));
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+
 const winston = require('winston');
-
 const chokidar = require('chokidar');
+const isDev = require('./lib/isDev');
 
-const watcher = chokidar.watch('./app');
-watcher.on('ready', () => {
-  watcher.on('all', () => {
-    winston.info('Clearing /app/ module cache from server');
-    Object.keys(require.cache).forEach((id) => {
-      // if (/[\/\\]app[\/\\]/.test(id)) delete require.cache[id];
-      if (/[/\\]app[/\\]/.test(id)) delete require.cache[id];
+if (isDev) {
+  const watcher = chokidar.watch('./app');
+  watcher.on('ready', () => {
+    watcher.on('all', () => {
+      winston.info('Clearing /app/ module cache from server');
+      Object.keys(require.cache).forEach((id) => {
+        if (/[/\\]app[/\\]/.test(id)) delete require.cache[id];
+      });
     });
   });
-});
+}
 
 const app = require('./app');
 
