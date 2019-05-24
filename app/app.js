676f39f721e4071773336d38baab3b4478b5b913
Yakult Yang <k@abreto.net>
2019-05-24 21:13:01 +0800

    set up logger and express

--- app/app.js
+++ app/app.js
@@ -14,11 +14,41 @@ class App {
   }
 
   initializeLogger() {
-    this.logger = new winston.Logger();
+    const { format, createLogger, transports } = winston;
+    this.logger = createLogger({
+      format: format.combine(
+        format.label({ label: '[yakult-weibo-server]' }),
+        format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        format.printf(info => `${info.label} [${info.timestamp}] [${info.level}]: ${info.message}`),
+      ),
+      transports: [
+        new transports.Console(),
+        // new winston.transports.File({ filename: 'combined.log' })
+      ],
+    });
   }
 
   initializeExpress() {
     this.http = express();
+
+    const app = this.http;
+
+    app.get('/api', (req, res) => res.send('API Route.'));
+
+    if (this.config.prod) {
+      app.use(express.static(this.config.clientDistPath));
+      app.get('/*', (req, res) => {
+        res.sendFile(this.config.clientDistHomepage);
+      });
+    }
+  }
+
+  listen() {
+    this.http.listen(this.config.port, () => {
+      this.logger.info(`🚀 Server listen on ${this.config.port}`);
+    });
   }
 }
 
