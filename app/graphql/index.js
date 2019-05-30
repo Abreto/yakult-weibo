707b54c136d9997f0efe474519dc678802040c2b
Yakult Yang <k@abreto.net>
2019-05-31 01:21:20 +0800

    hide sensitive error info

--- app/graphql/index.js
+++ app/graphql/index.js
@@ -1,5 +1,5 @@
 
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, ApolloError } = require('apollo-server-express');
 const schema = require('./schema');
 
 module.exports = app => new ApolloServer({
@@ -11,9 +11,13 @@ module.exports = app => new ApolloServer({
   }),
   formatError: (err) => {
     if (err.extensions.code === 'YW_EXPOSE') {
+      delete err.extensions.exception.stacktrace; //eslint-disable-line
       return err;
     }
     app.logger.error(err);
-    return new Error('Internal server error');
+    const ierr = new ApolloError('Internal server error', 'INTERNAL_SERVER_ERROR', {
+      message: 'Internal server error',
+    });
+    return ierr;
   },
 });
