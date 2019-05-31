80589811d0c12ad4bb9da80c64596a9f90a1bf11
Yakult Yang <k@abreto.net>
2019-05-31 17:24:59 +0800

    add service structure

---
+++ app/service/user.js
@@ -0,0 +1,12 @@
+
+module.exports = ({ model }) => ({
+  auth: async (token) => {
+    const parsed = Buffer.from(token, 'base64').toString();
+    const [user, pass] = parsed.split(':');
+    if (user === undefined || pass === undefined) return false;
+    return ((await model.user.where({
+      username: user,
+      password: pass,
+    }).count()) > 0);
+  },
+});
