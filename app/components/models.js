1cdaff5c57562a22c5bc9e37bbd417c93b8989d5
Yakult Yang <k@abreto.net>
2019-06-02 22:22:52 +0800

    reply model

--- app/components/models.js
+++ app/components/models.js
@@ -6,5 +6,6 @@ module.exports = (app) => {
     config: require('../model/config')(app),  // eslint-disable-line
     user: require('../model/user')(app),      // eslint-disable-line
     post: require('../model/post')(app),      // eslint-disable-line
+    reply: require('../model/reply')(app),    // eslint-disable-line
   });
 };
---
+++ app/model/reply.js
@@ -0,0 +1,13 @@
+
+module.exports = ({ mongoose }) => {
+  const { Schema } = mongoose;
+  const { ObjectId } = Schema.Types;
+
+  const replySchema = new Schema({
+    to: ObjectId,
+    poster: ObjectId,
+    content: String,
+  }, { timestamps: true });
+
+  return mongoose.module('Reply', replySchema);
+};
