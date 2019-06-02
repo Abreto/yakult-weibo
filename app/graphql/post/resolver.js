9c6769062c352de451e85a0fa22e14a6df00b018
Yakult Yang <k@abreto.net>
2019-06-02 21:38:33 +0800

    post logic

---
+++ app/graphql/post/resolver.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  Mutation: {
+    post: async (_, { content }, { logger, model, auth, checkPermission }) => {
+      checkPermission('MEMBER');
+
+      try {
+        const res = await model.post.create({
+          poster: auth.id,
+          content,
+        });
+        return (res !== null);
+      } catch (e) {
+        logger.warn(e);
+        return false;
+      }
+    },
+  },
+};
