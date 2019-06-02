fcc8b555324222bf6f950e2db5826404f027c90d
Yakult Yang <k@abreto.net>
2019-06-03 01:19:44 +0800

    remove useless comments

--- app/index.js
+++ app/index.js
@@ -3,9 +3,6 @@ const app = require('./app');
 
 module.exports = {
   boot: async () => {
-    // app.service.user.auth(Buffer.from('test:test').toString('base64')).then((res) => {
-    //   console.log(res);
-    // });
     await app.install();
     app.listen();
   },
